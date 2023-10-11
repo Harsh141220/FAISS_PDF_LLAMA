b4665d733711810e73ca9f7db7d6eb076ee97492
Harsh141220 <62830495+Harsh141220@users.noreply.github.com>
2023-10-11 12:43:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
-from dotenv import load_dotenv
 import os
+import sys
 from PyPDF2 import PdfReader
-import streamlit as st
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
 from langchain.llms import Replicate
+from langchain.callbacks import get_openai_callback
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.prompts import PromptTemplate
+import openai
+from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
+from transformers import AutoTokenizer,AutoModelForCausalLM
+
+
 
 # Load environment variables
-load_dotenv()
-replicate='r8_eDiBJIY40zorRGHfcMTNIHCcwcsOkyu1flmQS'
+env='gcp-starter'
+replicate='r8_RN2g#########################33' #your replicate key
 os.environ['REPLICATE_API_TOKEN'] = replicate
 
 def process_text(text):
     # Split the text into chunks using Langchain's CharacterTextSplitter
     text_splitter = CharacterTextSplitter(
         separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
+        chunk_size=500,
+        chunk_overlap=50,
         length_function=len
     )
     chunks = text_splitter.split_text(text)
@@ -31,40 +35,37 @@ def process_text(text):
     knowledgeBase = FAISS.from_texts(chunks, embeddings)
     
     return knowledgeBase
-
-def main():
-    st.title("Eucloid Data Solutions")
-    pdf = st.file_uploader('Upload your PDF Document', type='pdf')
     
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-        # Text variable will store the pdf text
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-        
-        # Create the knowledge base object
-        knowledgeBase = process_text(text)
-        
-        query = st.text_input('Ask a question to the PDF')
-        cancel_button = st.button('Cancel')
-        
-        if cancel_button:
-            st.stop()
-        
+pdf_reader = PdfReader('path to your pdf')
+text = ""
+for page in pdf_reader.pages:
+    text += page.extract_text()
+knowledgeBase = process_text(text)
+
+def resp(query,chat_history):        
         if query:
+            QUESTION_PROMPT = PromptTemplate.from_template("""Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+            Chat History:
+            {chat_history}
+            Follow Up Input: {query}
+            Standalone question:""")
             docs = knowledgeBase.similarity_search(query)
             llm = Replicate(
             model="a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
             input={"temperature": 0.5, "max_length": 5000,"top_p":0.9} 
             )
             chain = load_qa_chain(llm, chain_type='stuff')
-            
-            with get_openai_callback() as cost:
-                response = chain.run(input_documents=docs, question=query)
-                print(cost)
-                
-            st.write(response)
 
-if __name__ == "__main__":
-    main()
+            with get_openai_callback() as cost:
+                response = chain.run(input_documents=docs, question=query,chat_history=chat_history)                
+            return(response)
+            
+chat_history = []
+while True:
+    query = input('Prompt: ')
+    if query.lower() in ["exit", "quit", "q"]:
+        print('Exiting')
+        sys.exit()
+    result=resp(query,chat_history)
+    print('Answer: ' + result + '\n')
+    chat_history.append((query, result))
